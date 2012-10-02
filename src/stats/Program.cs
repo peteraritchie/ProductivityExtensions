0742b223f153117aa1a16b6b73718c1cdd9373f5
Peter Ritchie <github.com@peterRitchie.com>
2012-10-02 13:26:01 -0400

    First crack at something generate docs

--- src/stats/Program.cs
+++ src/stats/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Xml.XPath;
+using System.Xml.Xsl;
 using PRI.ProductivityExtensions.ReflectionExtensions;
 
 namespace stats
@@ -9,6 +13,11 @@ namespace stats
 	{
 		static void Main(string[] args)
 		{
+			if(args.Length > 1)
+			{
+				GenerateDocs();
+				return;
+			}
 			int mTally = 0;
 			int cTally = 0;
 			foreach (var extensionMethodCount in from type in typeof(Typeable).Assembly.GetTypes() where type.IsPublic && type.IsStatic() select type.GetMethods(BindingFlags.Static | BindingFlags.Public).Count(
@@ -20,6 +29,21 @@ namespace stats
 
 			Console.WriteLine("{0} classes, {1} methods.", cTally, mTally);
 			Console.ReadLine();
+		}
+
+		private static void GenerateDocs()
+		{
+			XPathDocument myXPathDoc = new XPathDocument(@"..\..\..\geschikt\bin\release\ProductivityExtensions.XML");
+			XslCompiledTransform myXslTrans = new XslCompiledTransform();
+			myXslTrans.Load(@"..\..\..\geschikt\bin\release\visual-studio-xml-doc.xsl");
+			using(var stream = new FileStream("doc.html", FileMode.Create, FileAccess.Write))//MemoryStream())
+			{
+				using(var writer = new StreamWriter(stream))
+				{
+					myXslTrans.Transform(myXPathDoc, null, writer);
+				}
+//				var text = Encoding.ASCII.GetString(stream.ToArray());
+			}
 		}
 	}
 }
