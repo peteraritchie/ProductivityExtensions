52341e530390b023b661e0c0e3b8302a680a0c93
peteraritchie <github.com@peterritchie.com>
2013-03-17 01:16:35 -0400

    non-generic version of reflection methods

--- src/geschikt/Reflection/TypeBuilderable.cs
+++ src/geschikt/Reflection/TypeBuilderable.cs
@@ -7,7 +7,7 @@ namespace PRI.ProductivityExtensions.ReflectionExtensions
 	public static partial class TypeBuilderable
 	{
 		/// <summary>
-		/// Add public field to <paramref name="typeBuilder"/> of name <paramref name="filedName"/>
+		/// Add public field to <paramref name="typeBuilder"/> of name <paramref name="fieldName"/>
 		/// </summary>
 		/// <typeparam name="T">Type of the field</typeparam>
 		/// <param name="typeBuilder"><seealso cref="TypeBuilder"/> that will have the new field</param>
@@ -15,11 +15,23 @@ namespace PRI.ProductivityExtensions.ReflectionExtensions
 		/// <returns>The new FieldBuilder object that encapsulates the field</returns>
 		public static FieldBuilder DefinePublicField<T>(this TypeBuilder typeBuilder, string fieldName)
 		{
-			return typeBuilder.DefineField(fieldName, typeof (T), FieldAttributes.Public);
+			return DefinePublicField(typeBuilder, fieldName, typeof (T));
 		}
 
 		/// <summary>
-		/// Add private field to <paramref name="typeBuilder"/> of name <paramref name="filedName"/>
+		/// Add public field to <paramref name="typeBuilder"/> of name <paramref name="fieldName"/>
+		/// </summary>
+		/// <param name="typeBuilder"><seealso cref="TypeBuilder"/> that will have the new field</param>
+		/// <param name="fieldName">Name of the field</param>
+		/// <param name="type">Type of the field</param>
+		/// <returns>The new FieldBuilder object that encapsulates the field</returns>
+		public static FieldBuilder DefinePublicField(this TypeBuilder typeBuilder, string fieldName, Type type)
+		{
+			return typeBuilder.DefineField(fieldName, type, FieldAttributes.Public);
+		}
+
+		/// <summary>
+		/// Add private field to <paramref name="typeBuilder"/> of name <paramref name="fieldName"/>
 		/// </summary>
 		/// <typeparam name="T">Type of the field</typeparam>
 		/// <param name="typeBuilder"><seealso cref="TypeBuilder"/> that will have the new field</param>
@@ -27,7 +39,19 @@ namespace PRI.ProductivityExtensions.ReflectionExtensions
 		/// <returns>The new FieldBuilder object that encapsulates the field</returns>
 		public static FieldBuilder DefinePrivateField<T>(this TypeBuilder typeBuilder, string fieldName)
 		{
-			return typeBuilder.DefineField(fieldName, typeof(T), FieldAttributes.Private);
+			return DefinePrivateField(typeBuilder, fieldName, typeof(T));
+		}
+
+		/// <summary>
+		/// Add private field to <paramref name="typeBuilder"/> of name <paramref name="fieldName"/>
+		/// </summary>
+		/// <param name="typeBuilder"><seealso cref="TypeBuilder"/> that will have the new field</param>
+		/// <param name="fieldName">Name of the field</param>
+		/// <param name="type">Type of the field</param>
+		/// <returns>The new FieldBuilder object that encapsulates the field</returns>
+		public static FieldBuilder DefinePrivateField(this TypeBuilder typeBuilder, string fieldName, Type type)
+		{
+			return typeBuilder.DefineField(fieldName, type, FieldAttributes.Private);
 		}
 
 		/// <summary>
@@ -40,7 +64,20 @@ namespace PRI.ProductivityExtensions.ReflectionExtensions
 		/// <returns>The new PropertyBuilder object that encapsulates the property</returns>
 		public static PropertyBuilder DefineProperty<T>(this TypeBuilder typeBuilder, string propertyName, PropertyAttributes propertyAttributes)
 		{
-			return typeBuilder.DefineProperty(propertyName, propertyAttributes, typeof (T), null);
+			return DefineProperty(typeBuilder, propertyName, typeof (T), propertyAttributes);
+		}
+
+		/// <summary>
+		/// Adds a public property to <paramref name="typeBuilder"/> of name <paramref name="propertyName"/>
+		/// </summary>
+		/// <param name="typeBuilder"><seealso cref="TypeBuilder"/> that will have the new property</param>
+		/// <param name="propertyName">Name of the property</param>
+		/// <param name="type">Type of the property</param>
+		/// <param name="propertyAttributes"><seealso cref="PropertyAttributes"/></param>
+		/// <returns>The new PropertyBuilder object that encapsulates the property</returns>
+		public static PropertyBuilder DefineProperty(this TypeBuilder typeBuilder, string propertyName, Type type, PropertyAttributes propertyAttributes)
+		{
+			return typeBuilder.DefineProperty(propertyName, propertyAttributes, type, null);
 		}
 
 		/// <summary>
@@ -63,12 +100,12 @@ namespace PRI.ProductivityExtensions.ReflectionExtensions
 		/// <param name="typeBuilder"><seealso cref="TypeBuilder"/> that will have the new property.</param>
 		/// <param name="propertyInfo"><seealso cref="PropertyInfo"/> to base name and the method.</param>
 		/// <returns>the new <seealso cref="MethodBuilder"/> object that encapsulates the method</returns>
-		public static MethodBuilder DefinePropertySetter<T>(this TypeBuilder typeBuilder, PropertyInfo propertyInfo)
+		public static MethodBuilder DefinePropertySetter(this TypeBuilder typeBuilder, PropertyInfo propertyInfo)
 		{
 			return typeBuilder.DefineMethod("set_" + propertyInfo.Name,
 			                                MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig,
 			                                null,
-			                                new[] {typeof (T)});
+			                                new[] {propertyInfo.PropertyType});
 		}
 	}
 }
