169829c98799767047bcd19e5a176c4448e1ad80
Peter Ritchie <github.com@peterRitchie.com>
2013-12-19 23:09:41 -0500

    Added reference to Microsoft.Bcl.1.1.6 and WebClient extensions

--- makenuget.bat
+++ makenuget.bat
@@ -1,5 +1,8 @@
+if not exist nuget\lib\net40-client md nuget\lib\net40-client
 copy src\geschikt\bin\Release\geschikt.dll nuget\lib\net40-client
+if not exist nuget\lib\net45 md nuget\lib\net45
 copy src\geschikt.net45\bin\Release\geschikt.dll nuget\lib\net45
+if not exist nuget\lib\sl4-wp md nuget\lib\sl4-wp 
 copy src\geschikt.wp7\Bin\Release\geschikt.wp7.dll nuget\lib\sl4-wp 
 pushd nuget
 ..\util\nuget.exe pack ProductivityExtensions.nuspec 
