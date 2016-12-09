d192fb808c62c5d87d579bd4154bd04a2e7ae69e
Peter Ritchie <peterritchie@quickenloans.com>
2016-12-09 16:35:18 -0500

    Updated assembly copyright

--- src/geschikt.net45/Properties/AssemblyInfo.cs
+++ src/geschikt.net45/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Peter Ritchie")]
 [assembly: AssemblyProduct("Productivity Extensions")]
-[assembly: AssemblyCopyright("Copyright © Peter Ritchie 2015")]
+[assembly: AssemblyCopyright("Copyright © Peter Ritchie 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.17.0")]
-[assembly: AssemblyFileVersion("2.0.17.0")]
+[assembly: AssemblyVersion("2.0.16.0")]
+[assembly: AssemblyFileVersion("2.0.16.0")]
--- src/geschikt/Properties/AssemblyInfo.cs
+++ src/geschikt/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Peter Ritchie")]
 [assembly: AssemblyProduct("Productivity Extensions")]
-[assembly: AssemblyCopyright("Copyright © Peter Ritchie 2015")]
+[assembly: AssemblyCopyright("Copyright © Peter Ritchie 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
