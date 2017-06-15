4d7ee1f63c40ac0a148df4ae7f091f96d9bfd2e6
Peter Ritchie <peter.ritchie@outlook.com>
2017-06-14 21:18:55 -0700

    updated build-related files

--- copynugetfiles.bat
+++ copynugetfiles.bat
@@ -4,4 +4,7 @@ copy src\geschikt\bin\Release\geschikt.pdb nuget\lib\net40-client
 if not exist nuget\lib\net45 md nuget\lib\net45
 copy src\geschikt.net45\bin\Release\geschikt.dll nuget\lib\net45
 copy src\geschikt.net45\bin\Release\geschikt.pdb nuget\lib\net45
+if not exist nuget\lib\netstandard1.3 md nuget\lib\netstandard1.3
+copy src\geschikt.netstandard\bin\Release\geschikt.dll nuget\lib\netstandard1.3
+copy src\geschikt.netstandard\bin\Release\geschikt.pdb nuget\lib\netstandard1.3
 nuget.exe pack nuget\ProductivityExtensions.nuspec -Symbols -version %APPVEYOR_BUILD_VERSION%
