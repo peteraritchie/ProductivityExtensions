02c556e758fbe268889883dda6ba78ced385d6f4
Peter Ritchie <peter.ritchie@outlook.com>
2017-06-15 10:32:05 -0700

    build update for .NET Standard

--- copynugetfiles.bat
+++ copynugetfiles.bat
@@ -5,6 +5,6 @@ if not exist nuget\lib\net45 md nuget\lib\net45
 copy src\geschikt.net45\bin\Release\geschikt.dll nuget\lib\net45
 copy src\geschikt.net45\bin\Release\geschikt.pdb nuget\lib\net45
 if not exist nuget\lib\netstandard1.3 md nuget\lib\netstandard1.3
-copy src\geschikt.netstandard\bin\Release\geschikt.dll nuget\lib\netstandard1.3
-copy src\geschikt.netstandard\bin\Release\geschikt.pdb nuget\lib\netstandard1.3
+copy src\geschikt.netstandard\bin\Release\netstandard1.3\geschikt.netstandard.dll nuget\lib\netstandard1.3
+copy src\geschikt.netstandard\bin\Release\netstandard1.3\geschikt.netstandard.pdb nuget\lib\netstandard1.3
 nuget.exe pack nuget\ProductivityExtensions.nuspec -Symbols -version %APPVEYOR_BUILD_VERSION%
